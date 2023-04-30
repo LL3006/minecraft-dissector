8881c7db83bf4fb42b384e2a58ce5ffc35033886
LL3006 <doublel3006@gmail.com>
2023-04-30 18:55:05 +0200

    Make generate_snippet() iterable

--- generate.js
+++ generate.js
@@ -26,165 +26,191 @@ function uncamel(s) {
     .join(" ")
 }
 
-const natives = {
-  container(args, types, { path }) {
-    return {
-      code: args
-        .map(({ type, name, anon }) => {
-          // if (name === 'item') console.error(type)
-          const new_path = `${path}_${name}`
-          const { code, hf_type } = generate_snippet(type, types, undefined, {
-            path: new_path,
-            name
-          })
-          hf.push({
-            name,
-            path: new_path,
-            type: hf_type || "FT_NONE",
-          })
-          return code
-        })
-        .join("\n"),
-    }
-  },
-  varint(args, types, { path }) {
-    return {
-      code: `minecraft_add_varint(tree, hf_${path}, tvb, &offset);`,
-      hf_type: "FT_UINT32",
-      return_type: "guint32"
-    }
-  },
-  string(args, types, { path }) {
-    return {
-      code: `minecraft_add_string(tree, hf_${path}, tvb, &offset);`,
-      hf_type: "FT_STRING",
-    }
-  },
-  bool(args, types, { path }) {
-    return {
-      code: `minecraft_add_bool(tree, hf_${path}, tvb, &offset);`,
-      hf_type: "FT_BOOLEAN",
-    }
-  },
-  u8(args, types, { path }) {
-    return {
-      code: `minecraft_add_u8(tree, hf_${path}, tvb, &offset);`,
-      hf_type: "FT_UINT8",
-    }
-  },
-  i8(args, types, { path }) {
-    return {
-      code: `minecraft_add_i8(tree, hf_${path}, tvb, &offset);`,
-      hf_type: "FT_INT8",
-    }
-  },
-  u16(args, types, { path }) {
-    return {
-      code: `minecraft_add_u16(tree, hf_${path}, tvb, &offset);`,
-      hf_type: "FT_UINT16",
-    }
-  },
-  i16(args, types, { path }) {
-    return {
-      code: `minecraft_add_i16(tree, hf_${path}, tvb, &offset);`,
-      hf_type: "FT_INT16",
-    }
-  },
-  i32(args, types, { path }) {
-    return {
-      code: `minecraft_add_i32(tree, hf_${path}, tvb, &offset);`,
-      hf_type: "FT_INT32",
-    }
-  },
-  i64(args, types, { path }) {
-    return {
-      code: `minecraft_add_i64(tree, hf_${path}, tvb, &offset);`,
-      hf_type: "FT_INT64",
-    }
-  },
-  f32(args, types, { path }) {
-    return {
-      code: `minecraft_add_f32(tree, hf_${path}, tvb, &offset);`,
-      hf_type: "FT_FLOAT",
-    }
-  },
-  f64(args, types, { path }) {
-    return {
-      code: `minecraft_add_f64(tree, hf_${path}, tvb, &offset);`,
-      hf_type: "FT_DOUBLE",
-    }
-  },
-  UUID(args, types, { path }) {
-    return {
-      code: `minecraft_add_UUID(tree, hf_${path}, tvb, &offset);`,
-      hf_type: "FT_BYTES",
-    }
-  },
-  restBuffer(args, types, { path }) {
-    return {
-      code: `minecraft_add_restbuffer(tree, hf_${path}, tvb, &offset);`,
-      hf_type: "FT_BYTES",
-    }
-  },
-  buffer({ countType }, types, {path, name }) {
-    const count = generate_snippet(countType, types, undefined, { path: `${path}_len`})
-
-    hf.push({
-      name: name ? `${name}Length` : 'Length',
-      path: `${path}_len`,
-      type: count.hf_type,
-    })
-
-    return {
-      code:
-`${count.return_type} ${path}_len = ${count.code}
-minecraft_add_buffer(tree, hf_${path}, tvb, &offset, ${path}_len);`,
-      hf_type: "FT_BYTES",
-    }
-  },
-  option(args, types, ctx) {
-    const inner = generate_snippet(args, types, undefined, ctx)
-    return {
-      code:
-`if (tvb_get_guint8(tvb, offset) == 1) {
-  offset += 1;
-  ${inner.code}
-}`,
-      hf_type: inner.hf_type,
-    }
-  },
-}
-
 class UnsupportedError extends Error {
   constructor(message) {
     super(message);
     this.name = "UnsupportedError";
   }
 }
-  
+
+function* generate_snippet(type, path, types, name, data, tree = "tree") {
+  let field_info
+  if (typeof type !== "string") field_info = type;
+  else {
+    if (!(type in types)) throw new UnsupportedError("Unknown type " + type)
+    field_info = types[type];
+  }
+
+  if (typeof field_info === "function") yield* field_info({path, types, name, data, tree})
+  else if (typeof field_info === "string") {
+    if (field_info === "native") throw new UnsupportedError("Unknown native type " + type)
+    yield* generate_snippet(field_info, path, types, name, data)
+  }
+  else if (Array.isArray(field_info))
+    yield* generate_snippet(field_info[0], path, types, name, field_info[1])
+  else throw new UnsupportedError("Invalid type " + type)
+}
+
+function merge_snippet(iterator) {
+  let code = ""
+  try {
+    for (const item of iterator) {
+      code += item.code + "\n"
+    } 
+  } catch (e) {
+      if (!(e instanceof UnsupportedError)) throw e;
+      code += `// [STUB]: ${e.message}\n`
+    }
+  return code.trim();
+}
+
+
+const natives = {
+  *container({path, types, data, tree}) {
+      for (let item of data) {
+        const new_path = `${path}_${item.name}`        
+        yield* generate_snippet(item.type, new_path, types, item.name, tree)
+      }
+    },
+  *varint({path, name, tree}) {
+    const hf_type =  "FT_UINT32"
+    hf.push({ name, path, type: hf_type})
+    yield {
+      code: `minecraft_add_varint(${tree}, hf_${path}, tvb, &offset);`,
+      hf_type,
+      return_type: "guint32"
+    }
+  },
+  *string({path, name, tree}) {
+    const hf_type =  "FT_STRING"
+    hf.push({ name, path, type: hf_type})
+    yield {
+      code: `minecraft_add_string(${tree}, hf_${path}, tvb, &offset);`,
+      hf_type,
+    }
+  },
+  *bool({path, name, tree}) {
+    const hf_type =  "FT_BOOLEAN"
+    hf.push({ name, path, type: hf_type})
+    yield {
+      code: `minecraft_add_bool(${tree}, hf_${path}, tvb, &offset);`,
+      hf_type,
+    }
+  },
+  *u8({path, name, tree}) {
+    const hf_type =  "FT_UINT8"
+    hf.push({ name, path, type: hf_type})
+    yield {
+      code: `minecraft_add_u8(${tree}, hf_${path}, tvb, &offset);`,
+      hf_type,
+    }
+  },
+  *i8({path, name, tree}) {
+    const hf_type =  "FT_INT8"
+    hf.push({ name, path, type: hf_type})
+    yield {
+      code: `minecraft_add_i8(${tree}, hf_${path}, tvb, &offset);`,
+      hf_type,
+    }
+  },
+  *u16({path, name, tree}) {
+    const hf_type =  "FT_UINT16"
+    hf.push({ name, path, type: hf_type})
+    yield {
+      code: `minecraft_add_u16(${tree}, hf_${path}, tvb, &offset);`,
+      hf_type,
+    }
+  },
+  *i16({path, name, tree}) {
+    const hf_type =  "FT_INT16"
+    hf.push({ name, path, type: hf_type})
+    yield {
+      code: `minecraft_add_i16(${tree}, hf_${path}, tvb, &offset);`,
+      hf_type,
+    }
+  },
+  *i32({path, name, tree}) {
+    const hf_type =  "FT_INT32"
+    hf.push({ name, path, type: hf_type})
+    yield {
+      code: `minecraft_add_i32(${tree}, hf_${path}, tvb, &offset);`,
+      hf_type,
+    }
+  },
+  *i64({path, name, tree}) {
+    const hf_type =  "FT_INT64"
+    hf.push({ name, path, type: hf_type})
+    yield {
+      code: `minecraft_add_i64(${tree}, hf_${path}, tvb, &offset);`,
+      hf_type,
+    }
+  },
+  *f32({path, name, tree}) {
+    const hf_type =  "FT_FLOAT"
+    hf.push({ name, path, type: hf_type})
+    yield {
+      code: `minecraft_add_f32(${tree}, hf_${path}, tvb, &offset);`,
+      hf_type,
+    }
+  },
+  *f64({path, name, tree}) {
+    const hf_type =  "FT_DOUBLE"
+    hf.push({ name, path, type: hf_type})
+    yield {
+      code: `minecraft_add_f64(${tree}, hf_${path}, tvb, &offset);`,
+      hf_type,
+    }
+  },
+  *UUID({path, name, tree}) {
+    const hf_type =  "FT_BYTES"
+    hf.push({ name, path, type: hf_type})
+    yield {
+      code: `minecraft_add_UUID(${tree}, hf_${path}, tvb, &offset);`,
+      hf_type,
+    }
+  },
+  *restBuffer({path, name, tree}) {
+    const hf_type =  "FT_BYTES"
+    hf.push({ name, path, type: hf_type})
+    yield {
+      code: `minecraft_add_restbuffer(${tree}, hf_${path}, tvb, &offset);`,
+      hf_type,
+    }
+  },
+  *buffer({path, name, data, types, tree}) {
+    if (data.countType === undefined) throw new UnsupportedError("Unknown buffer with countType not defined")
+    const iterator = generate_snippet(data.countType,`${path}_len`, types, name ? `${name}Length` : 'Length', undefined, tree)
+    const count = iterator.next().value
+    
+    const hf_type = "FT_BYTES"
+    hf.push({name, path, type: hf_type })
+    
+    yield {
+      code: `${count.return_type} ${path}_len = ${count.code}
+minecraft_add_buffer(${tree}, hf_${path}, tvb, &offset, ${path}_len);`,
+      hf_type
+    }
+  },
+  *option({path, name, data, types, tree}) {
+    let iterator = generate_snippet(data, path, types, name, undefined, tree)
+    let inner = iterator.next().value;
+    let code = `if (tvb_get_guint8(tvb, offset) == 1) {
+  offset += 1;
+`
+    code += indent(inner.code, 2)
+    let snippet = merge_snippet(iterator)
+    if (snippet) code += "\n" + indent(snippet, 2)
+    code += `
+}`
+    yield {
+      code,
+      hf_type: inner.hf_type,
+    }
+  },
+}
+
 const functions = []
 const hf = []
-
-function generate_snippet(type, types, args, ctx = {}) {
-  let fieldInfo
-  if (typeof type === "string") {
-    if (!(type in types)) throw new UnsupportedError("Unknown type " + type)
-
-    fieldInfo = types[type]
-  } else fieldInfo = type
-
-  if (typeof fieldInfo === "function") {
-    return fieldInfo(args, types, ctx)
-  }
-  if (typeof fieldInfo === "string") {
-    if (fieldInfo === "native") {
-      throw new UnsupportedError("Unknown native " + type)
-    }
-    return generate_snippet(fieldInfo, types, args, ctx)
-  } else if (Array.isArray(fieldInfo)) {
-    return generate_snippet(fieldInfo[0], types, fieldInfo[1], ctx)
-  } else throw new UnsupportedError("Invalid type " + type)
-}
 
 function types_from_namespace(namespace) {
   const types = {}
@@ -219,33 +245,21 @@ function generate(namespace) {
 
   const path = namespace.join("_")
 
-  functions.push(
-    `void ${path}(guint32 packet_id, tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, guint offset, guint32 len) {
+  let code = `void ${path}(guint32 packet_id, tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, guint offset, guint32 len) {
   switch (packet_id) {
-${indent(
-    Object.entries(names)
-      .map(([id, name]) => {
-        let code
-        try {
-          snippet = generate_snippet(names_to_types[name], TYPES, undefined, {
-            path: `${path}_${name}`,
-          })
-          code = snippet.code
-        } catch (e) {
-          if (!(e instanceof UnsupportedError)) throw e;
-          code = `// [STUB]: ${e.message}`
-        }
-
-        return `case ${id}:
-  col_set_str(pinfo->cinfo, COL_INFO, "${unsnake(name)} [${namespace[0]}] (${namespace[1]})");
-${indent(code)}
-  break;`
-      }, "    ")
-      .join("\n")
-  )}
+`
+  for (const [id, name] of Object.entries(names) ) {
+    code += indent(`case ${id}:
+  col_set_str(pinfo->cinfo, COL_INFO, "${unsnake(name)} [${namespace[0]}] (${namespace[1]})");`,4)
+    code += "\n"
+    let snippet = merge_snippet(generate_snippet(names_to_types[name], `${path}_${name}`, TYPES, name))
+    if (snippet) code += `${indent(snippet,6)}\n`
+    code += indent(`break;`,6)
+    code += "\n"
   }
+  code += `${indent(`}`)}
 }`
-  )
+  functions.push(code)
 }
 
 generate(["handshaking", "toServer"])
